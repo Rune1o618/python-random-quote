d9054ea1325f1767cfc6a1baa09e6fe2bd080876
Rune1o618 <abhijay.hazarika@gmail.com>
2020-09-15 12:39:14 +0530

    Reading form a file

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,14 @@
 def primary(): #Because its not C or C++
-  print("Keep it logically awesome.")
+ # print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt") #Opens text file and puts it in a variable f
+  quotes = f.readlines() #in variable quotes readlines function
+  f.close()              #format.close
+                         #quotes is an array, so we are calling the first
+                         #element of that array
 
-  #print(quotes)
+  print(quotes[0])          #print quotes the 0th element of the arrayx
 
 if __name__== "__main__":
   primary()
+
